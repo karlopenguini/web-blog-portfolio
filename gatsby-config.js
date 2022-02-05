d7298906e1101ad0e756ba75d04b2ce8750b33f4
karlo <karlobpalisoc@gmail.com>
2022-02-05 19:43:23 +0800

    change url

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
 				// Field under which the remote schema will be accessible. You'll use this in your Gatsby query
 				fieldName: "portfolio_data",
 				// Url to query from
-				url: "https://karlopalisoc-blog-content.herokuapp.com/graphql",
+				url: "https://karlopalisoc-blog-content.herokuapp.com",
 			},
 		},
 		{
